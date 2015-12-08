c6544f36a98bee8d70d8205397a0566ecc1efe17
scudsbook <monicazheng@Monicas-MacBook-Pro-2.local>
2015-12-08 11:06:26 -0500

    change
    1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="scudsbook.css">
+    <title>Scudsbook</title>
+</head>
+<body>
+  <div class="container">
+      <nav>
+        <ul>
+           <li class="navbar"><a href="">Log in</a></li>
+           <li class="navbar"><a href="">Cart</a></li>
+
+        </ul>
+      </nav>
+
+      <div>
+
+      <label for="searchbox">
+        <input type="text" id="searchbox" value="search for..."/>
+      </label>
+      <button type="submit">search</button>
+      </div>
+  </div>
+
+</body>
+</html>
