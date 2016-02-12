7d3a83c0e7f48a7fca4b06beb6e8cc02059b3188
Ye Chen <ye1.chen@sta.samsung.com>
2016-02-11 19:05:15 -0600

    Map update

--- MapPage/sc_map_main_page.php
+++ MapPage/sc_map_main_page.php
@@ -9,23 +9,23 @@
     <title>Scudsbook</title>
 
     <style>
-        body{
-            font-family:arial;
-            font-size:.8em;
+        body {
+            font-family: arial;
+            font-size: .8em;
         }
 
-        input[type=text]{
-            padding:0.5em;
-            width:20em;
+        input[type=text] {
+            padding: 0.5em;
+            width: 20em;
         }
 
-        input[type=submit]{
-            padding:0.4em;
+        input[type=submit] {
+            padding: 0.4em;
         }
 
-        #gmap_canvas{
-            width:100%;
-            height:70em;
+        #gmap_canvas {
+            width: 100%;
+            height: 70em;
         }
     </style>
 
@@ -33,63 +33,62 @@
 <body>
 
 <?php
-if($_POST){
-
-    include_once ('../Database/Database_Connection.php');
-    $databaseConnection = new Database_Connection();
-    $databaseConnection->createDatabase();
+if ($_POST) {
     // get latitude, longitude and formatted address
-    $data_arr = geocode($_POST['searchbox']);
+    $sc_zip = $_POST['searchbox'];
 
-    // if able to geocode the address
-    if($data_arr){
+    include_once('../Database/Store_Information.php');
+    $store_info = new Store_Information();
+    $store_info->queryAllStoreInfo();
+    $index = count($store_info->_store_name);
+    $counter_php = 1;
 
-        $latitude = $data_arr[0];
-        $longitude = $data_arr[1];
-        $formatted_address = $data_arr[2];
+    $latitude = $store_info->_store_location_lat[1];
+    $longitude = $store_info->_store_location_lan[1];
+    $formatted_address = $store_info->_default_address_street[1] . " " . $store_info->_default_address_city[1]
+        . " " . $store_info->_default_address_state[1] . " " . $store_info->_default_address_zip[1];
+    ?>
 
-        include_once('../Database/Store_Information.php');
-        $store_info = new Store_Information();
-        $store_info->queryAllStoreInfo();
-        ?>
+    <!-- google map will be shown here -->
+    <div id="gmap_canvas">Loading map...</div>
 
-        <!-- google map will be shown here -->
-        <div id="gmap_canvas">Loading map...</div>
+    <!-- JavaScript to show google map -->
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+    <script type="text/javascript">
+        function init_map() {
+            var myOptions = {
+                zoom: 14,
+                center: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var latitude = <?php echo json_encode($store_info->_store_location_lat); ?>;
+            var longitude = <?php echo json_encode($store_info->_store_location_lan); ?>;
+            var address_street = <?php echo json_encode($store_info->_default_address_street); ?>;
+            max_address=<?php echo $index; ?>;
 
-        <!-- JavaScript to show google map -->
-        <script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
-        <script type="text/javascript">
-            function init_map() {
-                var myOptions = {
-                    zoom: 14,
-                    center: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>),
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+            map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+            for (i = 1; i <=max_address; i++) {
                 marker = new google.maps.Marker({
                     map: map,
-                    position: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>)
+                    position: new google.maps.LatLng(latitude[i], longitude[i])
                 });
                 infowindow = new google.maps.InfoWindow({
-                    content: "<?php echo $formatted_address; ?>"
+                    content: address_street[i]
                 });
                 google.maps.event.addListener(marker, "click", function () {
                     infowindow.open(map, marker);
                 });
                 infowindow.open(map, marker);
+                <?php $counter_php++; ?>
             }
-            google.maps.event.addDomListener(window, 'load', init_map);
-        </script>
+        }
+        google.maps.event.addDomListener(window, 'load', init_map);
+    </script>
 
-        <?php
+    <?php
 
-        // if unable to geocode the address
-    }else{
-        echo "No map found.";
-    }
 }
 ?>
-
 
 
 </body>
---
+++ store_page.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ye1.chen
+ * Date: 2/11/16
+ * Time: 5:40 PM
+ */
